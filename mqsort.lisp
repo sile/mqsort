9cfb7c7245b9cd8d170381207c181d25db44ae90
Takeru Ohta <phjgt308@gmail.com>
2010-10-02 19:58:03 +0900

    README記述

--- mqsort.lisp
+++ mqsort.lisp
@@ -1,4 +1,13 @@
+(defpackage mqsort
+  (:use :common-lisp)
+  (:shadow :common-lisp sort)
+  (:export sort))
 (in-package :mqsort)
+
+(defvar *fastest* '(optimize (speed 3) (safety 0) (debug 0)))
+(defvar *interface* '(optimize (speed 3) (safety 1) (debug 1)))
+(deftype array-index () `(mod ,array-dimension-limit))
+(deftype most-efficient-string () '(simple-array character))
 
 (declaim (inline sort sv-sort partition set-pivot-at-front swap-range swap-if-greater))
 
